7b2f4c7ccc040d5dccad5a44639fe1676e9d48d8
miroslav.kovac <miroslav.kovac@pantheon.tech>
2020-11-10 17:15:40 +0100

    add before population and test that
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

--- my_app.py
+++ my_app.py
@@ -4,6 +4,18 @@ app = Flask(__name__)
 r = redis.Redis(host='localhost', port=6379)
 r.set('foo1', 'bar')
 
+@app.route('/test2')
+def test2():
+    return r.get('a').decode('utf-8')
+
+@app.route('/test3')
+def test3():
+    return r.get('b').decode('utf-8')
+
+@app.route('/test4')
+def test4():
+    return r.get('c').decode('utf-8')
+
 @app.route('/test')
 def test():
     return r.get('foo1').decode('utf-8')
--- test_flask.py
+++ test_flask.py
@@ -7,5 +7,10 @@ class TestFlaskMethods(unittest.TestCase):
     def test_flask(self):
         self.assertEqual(app.test(), 'bar')
 
+    def test_flask2(self):
+        self.assertEqual(app.test2(), 'a')
+        self.assertEqual(app.test3(), 'b')
+        self.assertEqual(app.test4(), 'c')
+
 if __name__ == '__main__':
     unittest.main()
