40d9a189a7252b8d29d9a9597d14359ae7b94cc1
miroslav.kovac <miroslav.kovac@pantheon.tech>
2020-11-10 16:57:01 +0100

    Fix test
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

--- test_default.py
+++ test_default.py
@@ -8,7 +8,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
         r = redis.Redis(host='localhost', port=6379)
         r.set('foo', 'bar')
-        self.asserEqual(r.get('foo'), 'bar')
+        self.assertEqual(r.get('foo'), 'bar')
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
